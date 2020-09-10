0a33c2cfc87a7237a3f7f3bd0181ef225a15beb9
Aleena Saleem <42008808+AleenaSaleem@users.noreply.github.com>
2020-09-10 15:15:38 +0530

    add methods

--- Statistics/Statistics.cs
+++ Statistics/Statistics.cs
@@ -1,12 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Statistics
 {
-    public class StatsComputer
+    public struct ComputedStats
     {
-        public Stats CalculateStatistics(List<float> numbers) {
-            //Implement statistics here
+        public float average;
+        public float Min;
+        public float Max;
+        public ComputedStats(float average,float Min,float Max)
+        {
+            this.average = average;
+            this.Min = Min;
+            this.Max = Max;
+        }
+    }
+    class StatisticsComputer
+    {
+        public static ComputedStats ComputeStatistics(List<float> data)
+        {
+            ComputedStats computedValues = new ComputedStats();
+            if (data.Count != 0)
+            {
+                computedValues.average = data.Average();
+                computedValues.Min = data.Min();
+                computedValues.Max = data.Max();
+            }
+            else
+            {
+                computedValues.average = float.NaN;
+                computedValues.Min = float.NaN;
+                computedValues.Max = float.NaN;
+            }
+            return computedValues;
         }
     }
 }
+
