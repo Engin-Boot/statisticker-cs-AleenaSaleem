be2337c2037d7748367cd1c87d3f8b558b4120d4
Aleena Saleem <42008808+AleenaSaleem@users.noreply.github.com>
2020-09-10 15:33:08 +0530

    update

--- Statistics/Statistics.cs
+++ Statistics/Statistics.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,30 +8,30 @@ namespace Statistics
 {
     public struct ComputedStats
     {
-        public float average;
+        public float Average;
         public float Min;
         public float Max;
         public ComputedStats(float average,float Min,float Max)
         {
-            this.average = average;
+            this.Average = average;
             this.Min = Min;
             this.Max = Max;
         }
     }
-    public class StatisticsComputer
+    class StatisticsComputer
     {
         public static ComputedStats ComputeStatistics(List<float> data)
         {
             ComputedStats computedValues = new ComputedStats();
             if (data.Count != 0)
             {
-                computedValues.average = data.Average();
+                computedValues.Average = data.Average();
                 computedValues.Min = data.Min();
                 computedValues.Max = data.Max();
             }
             else
             {
-                computedValues.average = float.NaN;
+                computedValues.Average = float.NaN;
                 computedValues.Min = float.NaN;
                 computedValues.Max = float.NaN;
             }
