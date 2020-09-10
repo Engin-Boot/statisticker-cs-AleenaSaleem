701cbfd8912e069f763b63cf68d2e4a3b37c13f9
Aleena Saleem <42008808+AleenaSaleem@users.noreply.github.com>
2020-09-10 15:25:37 +0530

    update

--- Statistics.Test/StatsUnitTest.cs
+++ Statistics.Test/StatsUnitTest.cs
@@ -9,22 +9,22 @@ namespace Statistics.Test
         [Fact]
         public void ReportsAverageMinMax()
         {
-            var statsComputer = new StatsComputer();
-            var computedStats = statsComputer.CalculateStatistics(
-                new List<___>{1.5, 8.9, 3.2, 4.5});
+            var statsComputer = new StatisticsComputer();
+            var computedValues = statsComputer.ComputeStatistics(
+                new List<___> { 1.5, 8.9, 3.2, 4.5 });
             float epsilon = 0.001F;
-            Assert.True(Math.Abs(statsComputer.average - 4.525) <= epsilon);
-            Assert.True(Math.Abs(statsComputer.max - 8.9) <= epsilon);
-            Assert.True(Math.Abs(statsComputer.min - 1.5) <= epsilon);
+            Assert.True(Math.Abs(computedValues.Average - 4.525) <= epsilon);
+            Assert.True(Math.Abs(computedValues.Max - 8.9) <= epsilon);
+            Assert.True(Math.Abs(computedValues.Min - 1.5) <= epsilon);
         }
         public void ReportsNaNForEmptyInput()
         {
-            var statsComputer = new StatsComputer();
-            var computedStats = statsComputer.CalculateStatistics(
-                new List<___>{});
-            //All fields of computedStats (average, max, min) must be
-            //Double.NaN (not-a-number), as described in
-            //https://docs.microsoft.com/en-us/dotnet/api/system.double.nan?view=netcore-3.1
+            var statsComputer = new StatisticsComputer();
+            var computedValues = statsComputer.ComputeStatistics(
+                new List<___> { });
+            Assert.True(float.IsNaN(computedValues.Average));
+            Assert.True(float.IsNaN(computedValues.Max));
+            Assert.True(float.IsNaN(computedValues.Min));
         }
     }
 }
